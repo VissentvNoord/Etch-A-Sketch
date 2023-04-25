9b969c9bc22c091af242571140853977f0125636
VissentvNoord <49689172+VissentvNoord@users.noreply.github.com>
2023-04-25 11:27:42 +0200

    Option for resolution setting, and random color.

--- grid.js
+++ grid.js
@@ -1,4 +1,34 @@
 const container = document.querySelector('#container');
+const resoBtn = document.querySelector('#btn');
+const resoWidth = 50;
+const resoHeight = 50;
+
+resoBtn.addEventListener('click', setResolution);
+
+function setResolution(){
+    let setResolution = prompt("resolution");
+    if(!isNumeric(setResolution)){
+        return console.log(setResolution + " is not a valid number.");
+    }
+    else{
+        if(setResolution <= 1){
+            return console.log("Resolution cant be 0 or lower!");
+        }
+
+        if(setResolution > 100){
+            setResolution = 100;
+        }
+
+        refreshGrid(setResolution);
+        return console.log("Refresh grid with a resolution of: " + setResolution);
+    }
+}
+
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+  }
 
 //First generate a row of the chosen width pixel amount. default 16.
 //Each element in the top row should have a display of flex(column).
@@ -34,9 +64,32 @@ function generateBox(column, height){
 
         //Adding hover event to the box
         box.addEventListener('mouseover', function(e) {
-            e.target.style.background = 'white';
+            setRandomColor(e.target);
         });
     }
+}
+
+function setRandomColor(target){
+    let randomCol = generateRandomColor();
+
+    target.style.background = randomCol;
+}
+
+function generateRandomColor(){
+    let maxVal = 0xFFFFFF; // 16777215
+    let randomNumber = Math.random() * maxVal; 
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);   
+    return `#${randColor.toUpperCase()}`
+}
+
+function refreshGrid(resolution){
+    //Deleting current elements from container.
+    container.innerHTML = '';
+
+    //Generating new grid
+    generateGrid(resolution, resolution);
 }
 
 generateGrid(50, 50);
